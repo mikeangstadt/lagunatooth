801cf61829faaf57e863e93268bd5631edd1a10b
Michael Angstadt <mangstadt@cbancnetwork.com>
2015-01-14 11:19:23 -0600

    Updated console messages.

--- boom.js
+++ boom.js
@@ -3,12 +3,14 @@ var btOBDReader = new OBDReader();
 var dataReceivedMarker = {};
 
 btOBDReader.on('dataReceived', function(data) {
+  console.log("Data Recieved from BMW...");
   console.log(data);
   dataReceivedMarker = data;
 });
 
 btOBDReader.on('connected', function() {
   //this.requestValueByName("vss"); //vss = vehicle speed sensor
+  console.log("Connected to BMW...")
 
   this.addPoller("vss");
   this.addPoller("rpm");
@@ -19,10 +21,11 @@ btOBDReader.on('connected', function() {
   this.addPoller("egt");
   this.addPoller("afr");
   this.addPoller("iat");
-  this.addPoller("Individual wheel speed sensors");
+  this.addPoller("iwss");
 
   this.startPolling(1000); //Request all values each second.
 });
 
 // Use first device with 'bmw' in the name
+console.log("Connecting to BMW...");
 btOBDReader.autoconnect('bmw');
